59de9315339a7f2250f2793a5db462733b4e7730
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-18 18:39:57 -0400

    clean

--- README.md
+++
@@ -1,1 +0,0 @@
-# eCommerceP2
---
+++ src/main/java/com/revature/ECommerceP2Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceP2Application.class, args);
+	}
+
+}
