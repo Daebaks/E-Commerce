316889c2a5a139c42af29046dff2c8139c2e04b7
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-20 15:02:28 -0400

    deleted Account

--- src/main/java/com/revature/data/AccountRepository.java
+++
@@ -1,5 +0,0 @@
-package com.revature.data;
-
-public class AccountRepository {
-
-}
--- src/main/java/com/revature/model/Order.java
+++ src/main/java/com/revature/model/Order.java
@@ -1,5 +1,40 @@
 package com.revature.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Entity
+@Table(name="order")
+@Data // generate getters/setter, toString, hashCode, and equals methods automatically
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
+	@Id
+	@Column(name = "order_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int order_id;
+	
+	//This will be filled by aggregating the junction table of order_customer and orders table
+	private Map<Integer, Integer> items_quantity;   //sku - quantity
+
+	public Order(Map<Integer, Integer> items_quantity) {
+		super();
+		this.items_quantity = items_quantity;
+	}
+	
+	
 }
