44ef066e8dc525965465558a0b17f484fca4a2c8
Jake Howell <jhowel44@uncc.edu>
2022-07-20 17:00:21 -0400

    What I be working on

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -1,5 +1,61 @@
 package com.revature.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name="users")
+@Data // generate getters/setter, toString, hashCode, and equals methods automatically
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ID;
+	
+	@NotBlank
+	@Length(min=5)
+	@Pattern(regexp = "[a-zA-Z][a-zA-Z0-9]*")
+	private String username;
+	
+	@NotBlank
+	private String password;
+	
+	@Email
+	private String email;
+	
+	// private List cart; //not sure what we are calling this part yet and not in the erd
+	
+	public User(@NotBlank @Length(min = 5) @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9]*") String username,
+			@NotBlank String password, @Email String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
