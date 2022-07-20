6ea9eeced4c6d3d01fed07a19c62fb29a90fcbe0
formula38 <louismconley@gmail.com>
2022-07-19 21:02:19 -0700

    Created Product Repository

--- src/main/java/com/revature/data/ProductRepository.java
+++ src/main/java/com/revature/data/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.revature.data;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.revature.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
