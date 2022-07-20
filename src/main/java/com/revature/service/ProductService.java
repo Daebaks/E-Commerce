1bbab10b56637bb13a8f83d444403e8b5d1b771f
formula38 <louismconley@gmail.com>
2022-07-20 12:07:42 -0700

    Removed bin directory and updated ProductService class

--- src/main/java/com/revature/service/ProductService.java
+++ src/main/java/com/revature/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.revature.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	ProductService productService;
