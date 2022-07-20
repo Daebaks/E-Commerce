205ac5f177a4ce16cfb7b4f3a29c5f82d45f0f2c
formula38 <louismconley@gmail.com>
2022-07-20 12:24:23 -0700

    Updates to Product Model class

--- src/main/java/com/revature/model/Product.java
+++ src/main/java/com/revature/model/Product.java
@@ -1,23 +1,39 @@
 package com.revature.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity(name = "product")
+@Entity
+@Table(name = "product")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
 	@Id
+	@Column(name = "sku")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long sku;
-	@NotNull(message = "Product name is required.")
+	
+	@Column(name = "name")
+	@NotBlank
 	private String name;
+	
+	@Column(name = "unitprice")
 	private Double unitprice;
+	
+	@Column(name = "category")
+	private String category;
 	
 	Product(String name, Double unitprice){
 		this.name = name;
