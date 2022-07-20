3c1cb3f450c07ae1f9d6686834939759e6f394cb
formula38 <louismconley@gmail.com>
2022-07-20 11:57:40 -0700

    Added Repository Stereotype Tag

--- src/main/java/com/revature/data/ProductRepository.java
+++ src/main/java/com/revature/data/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.revature.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
