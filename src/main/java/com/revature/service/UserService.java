cb10ed3ecfde0510d4f9c0411bd987dd750d4190
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-20 17:20:18 -0400

    added exceptions

--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.data.UserRepository;
+import com.revature.exception.UserNotFoundException;
 import com.revature.model.User;
 
 @Service
