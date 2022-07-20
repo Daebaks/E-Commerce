18a61e87f6372cb592525dbdf4c65790c89c5e5c
formula38 <louismconley@gmail.com>
2022-07-20 12:16:09 -0700

    Service Stereotype tag added to product service page

--- src/main/java/com/revature/service/ProductService.java
+++ src/main/java/com/revature/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.revature.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService {
 	
 	@Autowired
