f63c0c0a9d1b881fc79d11f3039e2db8941c0858
formula38 <louismconley@gmail.com>
2022-07-20 13:02:06 -0700

    Dependency Optimization

--- src/main/java/com/revature/model/Product.java
+++ src/main/java/com/revature/model/Product.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
