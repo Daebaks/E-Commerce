61f23426d39057f4affa693178bcc70db488b06b
formula38 <louismconley@gmail.com>
2022-07-19 21:07:13 -0700

    Created ProductController

--- src/main/java/com/revature/controller/ProductController.java
+++ src/main/java/com/revature/controller/ProductController.java
@@ -1,5 +1,13 @@
 package com.revature.controller;
 
+import com.revature.service.ProductService;
+
 public class ProductController {
+	
+	private ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
 
 }
