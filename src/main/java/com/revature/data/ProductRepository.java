a8e6087f53d6f76dd52549bbe44941f2d1b1706e
formula38 <louismconley@gmail.com>
2022-07-20 21:14:12 -0700

    Updates to ProductRepository and ProductService

--- src/main/java/com/revature/data/ProductRepository.java
+++ src/main/java/com/revature/data/ProductRepository.java
@@ -11,11 +11,15 @@ import com.revature.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	public Optional<Product> finByName(String name);
+	public Optional<Product> findByName(String name);
 	
 	public List<Product> findByCategory(String category);
 	
 	public List<Product> findBySku(Long sku);
 	
 	public List<Product> findByUnitprice(Double unitprice);
+
+	public boolean exists(Product sku);
+
+	public void deleteBySku(Product sku);
 }
