24ca365224277b50a2c8092dd20827278973b920
formula38 <louismconley@gmail.com>
2022-07-21 20:09:16 -0700

    changes needed to productservice

--- src/main/java/com/revature/service/ProductService.java
+++ src/main/java/com/revature/service/ProductService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.revature.data.ProductRepository;
