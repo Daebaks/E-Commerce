fdb4b063d0c77dfa0e9491cabfe5f708aeac323b
formula38 <louismconley@gmail.com>
2022-07-21 20:54:39 -0700

    Updates to ProductController

--- src/main/java/com/revature/controller/ProductController.java
+++ src/main/java/com/revature/controller/ProductController.java
@@ -1,18 +1,51 @@
 package com.revature.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.model.Product;
 import com.revature.service.ProductService;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("products")
+@CrossOrigin("*")
 public class ProductController {
 
 	@Autowired
 	ProductService productService;
 
-
+	@GetMapping
+	public List<Product> getAllProducts(){
+		return productService.findAll();
+	}
+	
+	@PostMapping
+	public Product addProduct(@RequestBody Product product) {
+		return productService.add(product);
+	}
+	
+	@GetMapping("available")
+	public List<Product> getAllAvailableProducts(){
+		return productService.getAvailable();
+	}
+	
+	@PutMapping
+	public Product updateProduct(@RequestBody Product product) {
+		return productService.update(product);
+	}
+	
+	@DeleteMapping
+	public boolean delete(@RequestBody Product product) {
+		return productService.delete(product);
+	}
 
 }
