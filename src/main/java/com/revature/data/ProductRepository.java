6e36a04c2900fcf4b0bf624b1470286cca54a0c4
formula38 <louismconley@gmail.com>
2022-07-21 20:41:38 -0700

    Updates to ProductRepository and ProductService classes

--- src/main/java/com/revature/data/ProductRepository.java
+++ src/main/java/com/revature/data/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.revature.data;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Product;
@@ -11,16 +11,13 @@ import com.revature.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
+	public Product getByProductSku(Long sku);
 	
-//	public Optional<Product> findByName(String name);
-//	
-//	public List<Product> findByCategory(String category);
-//	
-//	public List<Product> findBySku(Long sku);
-//	
-//	public List<Product> findByUnitprice(Double unitprice);
-//
-//	public boolean exists(Product sku);
-//
-//	public void deleteBySku(Product sku);
+	@Query("FROM product WHERE quantity > 0")
+	public List<Product> getAvailableProducts();
+	
+	public Product updateProduct(Product product);
+	
+	public Product addProduct();
+
 }
