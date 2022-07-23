8da0741815d22ace90ad7e2de147a0631ccfa66b
formula38 <louismconley@gmail.com>
2022-07-23 09:49:46 -0700

    JUnit Updates

--- src/test/java/com/revature/UserServiceTests.java
+++ src/test/java/com/revature/UserServiceTests.java
@@ -1,19 +1,32 @@
 package com.revature;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import com.revature.data.UserRepository;
+import com.revature.model.User;
+import com.revature.service.UserService;
+
 class UserServiceTests {
+	
+	private UserService us;
+	private UserRepository mockUr;
+	private User dummyUser;
 
 	@Before
-	static void setUp() throws Exception {
+	void setUp() {
+		us = new UserService();
+		mockUr = mock(UserRepository.class);
+		dummyUser = new User();
+		dummyUser.setId(0);
 	}
 
 	@After
-	static void tearDown() throws Exception {
+	void tearDown() {
 	}
 
 	@Test
