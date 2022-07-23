185c02a0f62f7d0082bffdade39f20d92ab3becf
formula38 <louismconley@gmail.com>
2022-07-23 08:34:55 -0700

    mvn successfully compiled and initiated Jacoco and sunfire. UserServiceTests and ProductServiceTests classes

---
+++ src/test/java/com/revature/UserServiceTests.java
@@ -0,0 +1,24 @@
+package com.revature;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTests {
+
+	@Before
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@After
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
