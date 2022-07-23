bf026f70d2ec7d5975e53ec2d14cfc6b3e0c2b5f
formula38 <louismconley@gmail.com>
2022-07-23 09:59:16 -0700

    JUnit Updates to UserServiceTests. May need to change cart type from List to LinkedList in User model class

--- src/test/java/com/revature/UserServiceTests.java
+++ src/test/java/com/revature/UserServiceTests.java
@@ -3,11 +3,15 @@ package com.revature;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import com.revature.data.UserRepository;
+import com.revature.model.Product;
 import com.revature.model.User;
 import com.revature.service.UserService;
 
@@ -22,6 +26,7 @@ class UserServiceTests {
 		us = new UserService();
 		mockUr = mock(UserRepository.class);
 		dummyUser = new User();
+		dummyUser.setCart(new LinkedList()<Product>());
 		dummyUser.setId(0);
 	}
 
@@ -34,6 +39,8 @@ class UserServiceTests {
 
 	@Test
 	void test() {
+		dummyUser = new User(0, "toddjones", "password1", "tjones@me.com", new LinkedList<Product>());
+		
 		fail("Not yet implemented");
 	}
 
