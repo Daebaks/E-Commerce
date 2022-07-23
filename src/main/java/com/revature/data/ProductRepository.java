bd407a52bf1e1a766d9519eb89076401c43c3e35
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-22 21:03:03 -0400

    test

--- src/main/java/com/revature/data/ProductRepository.java
+++ src/main/java/com/revature/data/ProductRepository.java
@@ -10,14 +10,5 @@ import com.revature.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	
-//	public Product getBySku(Long sku);
-	
-//	@Query("FROM products WHERE quantity > 0")
-//	public List<Product> getAvailable();
-//	
-//	public Product updateProduct(Product product);
-	
-//	public Product addProduct();
 
 }
