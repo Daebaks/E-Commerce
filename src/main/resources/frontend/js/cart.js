d22df42ea3e8e017ef35cf24a8da58154b92fac1
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-24 00:00:51 -0400

    populated products in cart page

---
+++ src/main/resources/frontend/js/cart.js
@@ -0,0 +1,34 @@
+let products;
+let cartContainer = document.getElementById("cart-container");
+let loggedUserId = sessionStorage.getItem("id");
+const URL = "http://localhost:8080";
+
+function fillCart(products) {
+  for (p of products) {
+    let divCart = document.createElement("div");
+
+    divCart.innerHTML = `
+        <h2>Category: ${p.category}</h2>
+        <h2>Name: ${p.name}</h2>
+        <h2>Price: ${p.unitprice}</h2>
+        Quantity<input type="number" id="${p.sku}" value="1" min="1" max="${p.quantity}"/><br/>
+        <button id="add-to-cart" value="${p.sku}" onclick="removeFromCart(this.value)">Remove from cart</button>
+        `;
+
+    divCart.setAttribute("class", "product");
+    cartContainer.append(divCart);
+  }
+}
+
+(async () => {
+  if (loggedUserId == null) {
+    window.location.href = "login.html";
+    alert("Log-in first to view your cart!!");
+  }
+  let req = await fetch(`${URL}/users/cart/${loggedUserId}`);
+  let res = await req.json();
+  products = res;
+  console.log(products);
+  cartContainer.innerHTML = "";
+  fillCart(products);
+})();
